21d9af63c2f709269d37fc4bcf13b28203943612
Tejas Kalpathi <60577394+TrexKalp@users.noreply.github.com>
2024-01-27 17:00:59 -0600

    removed react buddy

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,16 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
 // 1. import `ChakraProvider` component
-import {ChakraProvider} from "@chakra-ui/react";
-import {DevSupport} from "@react-buddy/ide-toolbox";
-import {ComponentPreviews, useInitial} from "./dev";
+import { ChakraProvider } from "@chakra-ui/react";
+import { DevSupport } from "@react-buddy/ide-toolbox";
+import { ComponentPreviews, useInitial } from "./dev";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-    <ChakraProvider>
-        <DevSupport ComponentPreviews={ComponentPreviews}
-                    useInitialHook={useInitial}
-        >
-            <App/>
-        </DevSupport>
-    </ChakraProvider>
+  <ChakraProvider>
+    <App />
+  </ChakraProvider>
 );
